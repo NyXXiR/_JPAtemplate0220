07207dcfb3070878f765969be651886a383c8fc0
heejaeyoon <skyminte88@naver.com>
2023-02-24 10:44:55 +0900

    c
    c

--- src/main/java/com/study/springboot/jdbc/MovieMapper.java
+++ src/main/java/com/study/springboot/jdbc/MovieMapper.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.study.springboot.entity.Movie;
+
 @Mapper
 public interface MovieMapper {
-	public List<Emp> findAllEmp
+	public List<Movie>findMovies
 	( @Param("search") String search, 
 			@Param("type") String type);
-	public List<Dept> findAllDept();
+
 }
