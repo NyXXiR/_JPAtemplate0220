29bc2fe9b0314e83d2eb955f9126539cba5e58a0
wodual <lgh0334@gmail.com>
2023-02-24 10:25:15 +0900

    mycontroller 수정
    .

--- src/main/java/com/study/springboot/jdbc/MovieMapper.java
+++ src/main/java/com/study/springboot/jdbc/MovieMapper.java
@@ -1,14 +1,14 @@
 package com.study.springboot.jdbc;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import com.study.springboot.entity.Genre;
+import com.study.springboot.entity.Movie;
 
 @Mapper
 public interface MovieMapper {
-	public List<Emp> findAllEmp
-	( @Param("search") String search, 
-			@Param("type") String type);
-	public List<Dept> findAllDept();
+  public List<Movie> findAllMovie(@Param("search") String search, @Param("type") String type);
+
+  public List<Genre> findAllGenre();
 }
