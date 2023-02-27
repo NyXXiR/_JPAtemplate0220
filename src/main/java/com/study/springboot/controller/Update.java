d50f416c617bc9488f7eac5578e33cf35db290f0
heejaeyoon <skyminte88@naver.com>
2023-02-27 14:12:20 +0900

    c
    c

--- src/main/java/com/study/springboot/controller/Update.java
+++ src/main/java/com/study/springboot/controller/Update.java
@@ -1,36 +1,30 @@
 package com.study.springboot.controller;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.study.springboot.entity.Movie;
 import com.study.springboot.repository.MovieRepository;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @Log4j2
+
 @RequiredArgsConstructor
-@RequestMapping("/view/*")
 public class Update {
-	final MovieRepository movieRepository;
-	
-	@GetMapping("/update/{mno}")
-	public String update(@PathVariable long mno, Model model) {
-		log.info("-----------------"+mno);
-	
-		Optional<Movie> movie = this.movieRepository.findById(mno);
-		log.info("-----------------"+movie);
-		
-		model.addAttribute("movie",movie);
-		return "update";
-	}
-	
+  final MovieRepository movieRepository;
+
+  @GetMapping("/update/{mno}")
+  public String update(@PathVariable long mno, Model model) {
+    log.info("-----------------" + mno);
+
+    Movie movie = this.movieRepository.findById(mno).orElseThrow();
+    log.info("-----------------" + movie);
+
+    model.addAttribute("movie", movie);
+    return "view/update";
+  }
+
 }
